ff7144ddc815e8b4f925ef3428e7fd450c639e73
ahmed-eishtawi <144367916+ahmed-eishtawi@users.noreply.github.com>
2024-12-12 21:24:41 +0200

    websocket logic finished

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,92 @@
-import Database from 'better-sqlite3';
-import path from 'path';
+import Database from "better-sqlite3";
+import path from "path";
+import fs from "fs";
 
-const dbPath = path.join(process.cwd(), 'db/awqaf-froms.db');
-const db = new Database(dbPath, { verbose: console.log });
+// Ensure the 'db' directory exists
+const dbDir = path.join(process.cwd(), "db");
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir);
+}
 
-db.prepare(`
-  CREATE TABLE IF NOT EXISTS users (
+// Define the database path
+const dbPath = path.join(dbDir, "awqaf-forms.db");
+
+// Create or connect to the database
+const db = new Database(dbPath);
+
+// Create tables
+db.prepare(
+  `
+  CREATE TABLE IF NOT EXISTS sides (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT
+    title TEXT NOT NULL
   )
-`).run();
+`
+).run();
+
+db.prepare(
+  `
+  CREATE TABLE IF NOT EXISTS forms (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    form_number INTEGER NOT NULL,
+    side_id INTEGER NOT NULL,
+    is_selected BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (side_id) REFERENCES sides(id) ON DELETE CASCADE
+  )
+`
+).run();
+
+/* this part of code will be deleted */
+/*
+    because of the short time to make this app works
+    i hard coded add the sides and forms to the database
+    and in the updates after the competition ends i will update it to be dynamic 
+*/
+
+// Insert sides data
+// const insertSide = db.prepare(`INSERT INTO sides (title) VALUES (?)`);
+// const sides = [
+//   "جانب أصول الطيبة كاملا",
+//   "جانب أصول الطيبة",
+//   "الشاطبية مع الذرة",
+//   "جانب المورد",
+//   "جانب العقيلة",
+//   "جانب حفظ الشاطبية كاملا",
+//   "جانب حفظ باب الأصول من الشاطبية",
+// ];
+// sides.forEach((side) => {
+//   try {
+//     insertSide.run(side);
+//   } catch (err) {
+//     console.error(`Error inserting side: ${side} - ${err.message}`);
+//   }
+// });
+
+// // Insert forms data
+// const insertForm = db.prepare(
+//   `INSERT INTO forms (form_number, side_id) VALUES (?, ?)`
+// );
+
+// const sideFormsData = {
+//   1: 3,
+//   2: 10,
+//   3: 12,
+//   4: 5,
+//   5: 20,
+//   6: 15,
+//   7: 24,
+// };
+
+// Object.entries(sideFormsData).forEach(([sideId, numForms]) => {
+//   for (let formNumber = 1; formNumber <= numForms; formNumber++) {
+//     try {
+//       insertForm.run(formNumber, sideId);
+//     } catch (err) {
+//       console.error(
+//         `Error inserting form for side_id ${sideId} with form_number ${formNumber} - ${err.message}`
+//       );
+//     }
+//   }
+// });
 
 export default db;
--- src/routes/socketRoute.js
+++ src/routes/socketRoute.js
@@ -1,12 +1,108 @@
+import db from "../config/database.js";
+
+/* */
+
 const useSocketIo = (io) => {
-  let my_data = [];
   io.on("connection", (socket) => {
     console.log("user connected");
 
-    socket.on("receive_data", (data) => {
-      console.log(data);
-      my_data.push(data);
-      io.emit("send_data", my_data);
+    /*  */
+    socket.on("get_all_sides", (data) => {
+      const { sides } = data;
+
+      let get_sides;
+      if (sides === "all") {
+        get_sides = db.prepare(`SELECT * FROM sides;`);
+      }
+      //
+      else if (typeof sides === "number") {
+        get_sides = db.prepare(`SELECT * FROM sides WHERE id = ?;`);
+      }
+      //
+      else {
+        io.emit("get_all_sides", "invalid side");
+        return;
+      }
+
+      try {
+        const result = sides === "all" ? get_sides.all() : get_sides.all(sides);
+        if (result.length > 0) {
+          io.emit("get_all_sides", result);
+        }
+        //
+        else {
+          io.emit("get_all_sides", "side not found");
+        }
+      } catch (err) {
+        console.error(`Error getting sides: ${err.message}`);
+      }
+    });
+
+    /*  */
+    socket.on("select_side", (data) => {
+      const { side_id } = data;
+
+      /* get the forms for specific side */
+
+      let side_forms = [];
+
+      const get_forms = db.prepare(
+        `SELECT form_number FROM forms WHERE side_id = ? AND is_selected = FALSE;`
+      );
+      try {
+        const result = get_forms.all(side_id);
+
+        if (result.length > 0) {
+          side_forms = result.map((form) => form.form_number);
+        }
+        //
+        else {
+          io.emit("get_selected_side", `side not found`);
+          return;
+        }
+      } catch (err) {
+        console.error(`Error getting forms: ${side_id} - ${err.message}`);
+      }
+
+      io.emit("get_selected_side", {
+        side_id,
+        side_forms,
+      });
+    });
+
+    /* */
+    socket.on("select_form", (data) => {
+      const { side_id, form_number } = data;
+      /* update the form to be selected */
+      const update_form = db.prepare(
+        `UPDATE forms SET is_selected = TRUE WHERE side_id = ? AND form_number = ?`
+      );
+      try {
+        const result = update_form.run(side_id, form_number);
+
+        /* if there is no changes in the database then the form was not found */
+        if (result.changes == 0) {
+          io.emit(
+            "get_selected_form",
+            `form not found: ${form_number} for side: ${side_id}`
+          );
+          return;
+        }
+      } catch (err) {
+        console.error(`Error updating form: ${side_id} - ${err.message}`);
+      }
+
+      /* emit the selected form */
+      io.emit("get_selected_form", {
+        side_id,
+        form_number,
+      });
+    });
+
+    /* */
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
   });
 };
